de73b6ff8a84163f1eebcda22dd508c6ee302714
Jazz Bozner <jazzbozner@gmail.com>
2019-11-16 19:15:37 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,8 @@ end
 def print_first_directors_movie_titles
   s_movies = directors_database[0][:movies]
   i = 0
-  binding.pry while i < s_movies.length do
+  binding.pry 
+  while i < s_movies.length do
     titles = s_movies[i][:title]
     puts titles
     i += 1
